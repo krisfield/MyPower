6faac52c9467d66a0beaf32b7ae2be8346915d66
Kris Field <kristopher.field@gmail.com>
2017-08-13 19:28:23 -0500

    More user input/cleaner db

--- mypower.py
+++ mypower.py
@@ -2,31 +2,34 @@ import os
 import sys
 
 import csv
+import datetime
 import sqlite3
-import urllib
+import urllib.request
 
 con = sqlite3.connect('mypower.db')
 con.text_factory = str
 cur = con.cursor()
-#DROPING TABLE OR ELSE IT JUST GROWS
-cur.execute("DROP TABLE offers")
 
-def connect():
+def clear():
+  os.system('cls' if os.name == 'nt' else 'clear')
+
+def dbBuild():
   #DOWNLOADING CSV FROM POWERTOCHOOSE.ORG AND SAVING FILE
   url = 'http://www.powertochoose.org/en-us/Plan/ExportToCsv'
-  urllib.urlretrieve(url, 'mypower.csv')
+  urllib.request.urlretrieve(url, 'mypower.csv')
 
   #CONNECTING TO DB
   con = sqlite3.connect('mypower.db')
-  con.text_factory = str
+  #con.text_factory = str
   cur = con.cursor()
-
+  #DROPING TABLE OR ELSE IT JUST GROWS
+  cur.execute("DROP TABLE IF EXISTS offers")
 
   #CREATING DB TABLE IF IT DOESN'T EXIST
-  cur.execute("CREATE TABLE IF NOT EXISTS offers ('idKey', 'TduCompanyName', 'RepCompany', 'Product', 'kwh500', 'kwh1000', 'kwh2000', 'Fees/Credits', 'PrePaid', 'TimeOfUse', 'Fixed', 'RateType', 'Renewable', 'TermValue', 'CancelFee', 'Website', 'SpecialTerms', 'TermsURL', 'Promotion', 'PromotionDesc', 'FactsURL', 'EnrollURL', 'PrepaidURL', 'EnrollPhone', 'NewCustomer', 'MinUsageFeesCredits', 'avgPrice');")
+  cur.execute("CREATE TABLE IF NOT EXISTS offers ('idKey' INTEGER, 'TduCompanyName', 'RepCompany', 'Product', 'kwh500' INTEGER, 'kwh1000' INTEGER, 'kwh2000' INTEGER, 'Fees/Credits', 'PrePaid', 'TimeOfUse', 'Fixed', 'RateType', 'Renewable' INTEGER, 'TermValue' INTEGER, 'CancelFee', 'Website', 'SpecialTerms', 'TermsURL', 'Promotion', 'PromotionDesc', 'FactsURL', 'EnrollURL', 'PrepaidURL', 'EnrollPhone', 'NewCustomer', 'MinUsageFeesCredits', 'avgPrice');")
 
   #OPENING DOWNLOADED CSV TO SAVE IT INTO DB
-  with open('mypower.csv','rb') as fin:
+  with open('mypower.csv','rt') as fin:
     dr = csv.DictReader(fin) # comma is default delimiter
     to_db = [(i['[idKey]'], i['[TduCompanyName]'], i['[RepCompany]'], i['[Product]'], i['[kwh500]'], i['[kwh1000]'], i['[kwh2000]'], i['[Fees/Credits]'], i['[PrePaid]'], i['[TimeOfUse]'], i['[Fixed]'], i['[RateType]'], i['[Renewable]'], i['[TermValue]'], i['[CancelFee]'], i['[Website]'], i['[SpecialTerms]'], i['[TermsURL]'], i['[Promotion]'], i['[PromotionDesc]'], i['[FactsURL]'], i['[EnrollURL]'], i['[PrepaidURL]'], i['[EnrollPhone]'], i['[NewCustomer]'], i['[MinUsageFeesCredits]']) for i in dr]
 
@@ -39,29 +42,68 @@ def connect():
   con.commit()
   con.close()
 
-def clear():
-  os.system('cls' if os.name =='nt' else 'clear')
+def dbCheck():
+  """Shows last time data was downloaded from Power To Choose and gives option to update"""
+  csv_last_download = datetime.datetime.fromtimestamp(os.stat('mypower.csv').st_mtime)
+  print ("This database was last updated from Power to Choose at {}\n\n".format(csv_last_download))
 
-def user_input():
-  """Enter your avg kwh usage"""
-  try:
-    ##Need to test to see if this error actully works
-    usage = int(input("Enter you average energy consumption in Kwh: "))
-  except ValueError:
-    print("Avg energy consumption must be a number")
-  else:
-    return usage
-  #Ask them to select current company or no company
-  #ask them if they want renewable
-  #Ask minimum term length
+  if input("Would you like to update this database? [y/N]: ").lower() == 'y':
+    print("Updating database...")
+    dbBuild()
 
-def home_screen():
+def user_input(TDU):
+  """Enter your preferences to find the best deal for your situation"""
+  user_pref = {}
+  while True:
+    try:
+      tdu_num = int(input("Enter number of your TDU: ")) - 1
+      ##need to have a check that this value is in range
+    except ValueError:
+      print("Oops! That was not a valid number. Try again...")
+    else:
+      user_pref["tdu"] = TDU[tdu_num]["tdu_name"]
+      break
+
+  while True:
+    try:
+      usage = int(input("Enter you average energy consumption in Kwh: "))
+    except ValueError:
+      print("Oops! That was not a valid number. Try again...")
+    else:
+      user_pref["usage"] = usage
+      break
+
+  while True:
+    try:
+      renewable = input("Do you want fully Renewable energy? [y/N]: ").lower()
+    except ValueError:
+      print("Oops! That was not a valid number. Try again...")
+    else:
+      if renewable != 'y':
+        user_pref["renewable"] = 0
+      else:
+        user_pref["renewable"] = 100
+    break
+
+  while True:
+    try:
+      contract = int(input("Desired minimum contract lenght (months) [0]?: ") or "0")
+    except ValueError:
+      print("Oops! That was not a valid number. Try again...")
+    else:
+      user_pref["contract term"] = contract
+      break
+
+  return user_pref
+
+
+def menu():
+  clear()
   print("""
 Welcome to MyPowerToChoose a script which makes finding the best deal on power easier than every. This program will weed out the misleading utility ads commonly seen on powertochoose.org.
-___________________________________________________
 """)
 
-def avgprice(avgKWH):
+def avgprice(USER):
   con = sqlite3.connect('mypower.db')
   con.text_factory = str
   cur = con.cursor()
@@ -74,42 +116,55 @@ def avgprice(avgKWH):
     kwh500 = float(row[4])
     idkey = row[0]
 
-    if avgKWH >= 1000:
-      price = ((avgKWH-1000) * kwh2000) + (500 * kwh1000) + (500 * kwh500)
+    if USER["usage"] >= 1000:
+      price = ((USER["usage"]-1000) * kwh2000) + (500 * kwh1000) + (500 * kwh500)
 
-    elif avgKWH >= 500:
-      price = ((avgKWH-500) * kwh1000) + (500 * kwh500)
+    elif USER["usage"] >= 500:
+      price = ((USER["usage"]-500) * kwh1000) + (500 * kwh500)
 
     else:
-      price = avgKWH * kwh500
+      price = USER["usage"] * kwh500
 
     cur.execute('UPDATE offers SET "avgPrice"=? WHERE "idKey"=?', (price, idkey))
     con.commit()
   con.close()
 
-def view_offers():
-  """View offers."""
+def build_tdu():
   con = sqlite3.connect('mypower.db')
   cur = con.cursor()
+  cur.execute('SELECT DISTINCT "TduCompanyName" FROM offers')
+  result = cur.fetchall()
+  i = 0
+  tdu = {}
+  print("Choose your TDU provider:")
+  print("_"*25)
+  for row in result:
+    tdu[i] = {'tdu_name': row[0]}
+    print('{}) {}'.format(i+1, row[0]))
+    i += 1
+  print("\n\n")
+  return tdu
+  con.close()
+
+def view_offers(USER):
+  """View offers."""
   clear()
+  con = sqlite3.connect('mypower.db')
+  cur = con.cursor()
   print("Company           ||Price:   ||Term ||Renewable ||Rate Type")
   print("_"*50)
   i = 1
-  ##Displaying Top Ten lowest prices based on power consumption
-  ##Add fees in red
-  ##Highlight in green if renewable
-  ##Allow user to go to next page and back
-  ##Show links to deals
-  #clean up table format
-  #Allow user to select the offer they want and the display all important info
-  for row in cur.execute('SELECT * FROM offers ORDER BY avgPrice ASC LIMIT 10'):
+
+  for row in cur.execute('SELECT * FROM offers WHERE TduCompanyName=? AND TermValue >=? AND Renewable >=? ORDER BY avgPrice ASC LIMIT 10', (USER["tdu"], USER["contract term"], USER["renewable"])):
     print("{}) {} || ${} || {} months || {}% || {} \n".format(i, row[2], row[26], row[13], row[12], row[11]))
     i += 1
   con.close()
 
 if __name__ == '__main__':
-  connect()
-  home_screen()
-  avgprice(user_input())
-  view_offers()
+  menu()
+  dbCheck()
+  tdu_choices = build_tdu()
+  preferences = user_input(tdu_choices)
+  avgprice(preferences)
+  view_offers(preferences)
 
